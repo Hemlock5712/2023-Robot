1cb332295de66a436832eb74a073c28d6d18f164
James Inman <jimmyinman2005@outlook.com>
2022-12-27 16:20:44 -0500

    Attempt to add safety for PPAstar

--- src/main/java/frc/robot/commands/PPAStar.java
+++ src/main/java/frc/robot/commands/PPAStar.java
@@ -44,28 +44,32 @@ public class PPAStar extends CommandBase {
   public void initialize() 
   {
     PathPlannerTrajectory trajectory;
-    
+
     // Due to repeateded passing of specified end angle as a Rotation2d object,
     // allocate one object and reference it.
+    Translation2d endTranslationObj = new Translation2d(endX, endY);
     Rotation2d endRotationObj = Rotation2d.fromDegrees(endAngle);
 
     Pose2d startPose = poseEstimatorSystem.getCurrentPose();
-    
+
+    // Already there.
+    if (startPose.getTranslation().equals(endTranslationObj))
+      return;
+
     List<Translation2d> internalPoints = Pathfinding.generatePath(startPose.getX(), startPose.getY(), endX, endY);
-    
+
     // Depending on if internal points are present, make a new array of the other
     // points in the path.
-    if (!internalPoints.isEmpty())
-    {
+    if (!internalPoints.isEmpty()) {
       // Declare an array to hold PathPoint objects made from all other points specified in constructor.
       PathPoint[] restOfPoints = new PathPoint[internalPoints.size() - 1];
 
-      PathPoint secondPoint = new PathPoint(internalPoints.get(0), internalPoints.get(1).minus(internalPoints.get(0)).getAngle(), endRotationObj);
+      PathPoint secondPoint = new PathPoint(internalPoints.get(0),
+          internalPoints.get(1).minus(internalPoints.get(0)).getAngle(), endRotationObj);
 
-      for (int i = 0; i < internalPoints.size() - 1; i++)
-      {
-        double y1 = internalPoints.get(i+1).getY();
-        double x1 = internalPoints.get(i+1).getX();
+      for (int i = 0; i < internalPoints.size() - 1; i++) {
+        double y1 = internalPoints.get(i + 1).getY();
+        double x1 = internalPoints.get(i + 1).getX();
         double y0 = internalPoints.get(i).getY();
         double x0 = internalPoints.get(i).getX();
         double hypotenuse = Math.hypot(y1 - y0, x1 - x0);
@@ -73,16 +77,18 @@ public class PPAStar extends CommandBase {
 
         restOfPoints[i] = new PathPoint(internalPoints.get(i + 1), angleOfTangentLineToXAxis, endRotationObj);
       }
-      restOfPoints[restOfPoints.length - 1] = new PathPoint(new Translation2d(endX, endY), endRotationObj, endRotationObj);
+      restOfPoints[restOfPoints.length - 1] = new PathPoint(endTranslationObj, endRotationObj, endRotationObj);
 
       trajectory = PathPlanner.generatePath(constraints,
-          new PathPoint(startPose.getTranslation(), internalPoints.get(0).minus(startPose.getTranslation()).getAngle(), driveSystem.getModulePositions()[0].angle),
+          new PathPoint(startPose.getTranslation(), internalPoints.get(0).minus(startPose.getTranslation()).getAngle(),
+              driveSystem.getModulePositions()[0].angle),
           secondPoint,
           restOfPoints);
     } else {
-          trajectory = PathPlanner.generatePath(constraints,
-          new PathPoint(startPose.getTranslation(), internalPoints.get(0).minus(startPose.getTranslation()).getAngle(), driveSystem.getModulePositions()[0].angle),
-          new PathPoint(new Translation2d(endX, endY), endRotationObj, endRotationObj));
+      trajectory = PathPlanner.generatePath(constraints,
+          new PathPoint(startPose.getTranslation(), internalPoints.get(0).minus(startPose.getTranslation()).getAngle(),
+              driveSystem.getModulePositions()[0].angle),
+          new PathPoint(endTranslationObj, endRotationObj, endRotationObj));
     }
 
     pathDrivingCommand = DrivetrainSubsystem.followTrajectory(driveSystem, poseEstimatorSystem, trajectory);
@@ -90,9 +96,18 @@ public class PPAStar extends CommandBase {
   }
 
   @Override
+  public boolean isFinished()
+  {
+    return (pathDrivingCommand == null || !pathDrivingCommand.isScheduled());
+  }
+
+  @Override
   public void end(boolean interrupted)
   {
-    pathDrivingCommand.cancel();
-    driveSystem.stop();
+    if (interrupted)
+    {
+      pathDrivingCommand.cancel();
+      driveSystem.stop();
+    }
   }
 }
