05a81cac64a5362897f77dcdbe139467177412ff
JosephTLockwood <joeojazz@gmail.com>
2023-01-05 23:50:03 -0500

    Attempt at adding auto nodes based off of obstacles

--- src/main/java/frc/robot/PathFinder/Obstacle.java
+++ src/main/java/frc/robot/PathFinder/Obstacle.java
@@ -9,13 +9,19 @@ public class Obstacle {
     this.polygon = new PolygonFloat(xPoints, yPoints, nPoints);
   }
 
-  public List<Node> getNodes() {
+  public List<Node> getNodes(double robotSize) {
     List<Node> nodes = new ArrayList<Node>();
-    for(int i = 0; i < this.polygon.npoints; i++) {
-      if(this.polygon.xpoints[i]!=0){
-        nodes.add(new Node(this.polygon.xpoints[i], this.polygon.ypoints[i]));
-      } 
+    for (int i = 0; i < this.polygon.npoints; i++) {
+        if (this.polygon.xpoints[i] != 0) {
+            // create a node at a distance from the obstacle equal to the size of the robot
+            double buffer = robotSize / 2;
+            nodes.add(new Node(this.polygon.xpoints[i] + buffer, this.polygon.ypoints[i] + buffer));
+            nodes.add(new Node(this.polygon.xpoints[i] - buffer, this.polygon.ypoints[i] - buffer));
+            nodes.add(new Node(this.polygon.xpoints[i] + buffer, this.polygon.ypoints[i] - buffer));
+            nodes.add(new Node(this.polygon.xpoints[i] - buffer, this.polygon.ypoints[i] + buffer));
+        }
     }
     return nodes;
   }
+
 }
