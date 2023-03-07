6057f638abba19d8852f7a935b078982e58ce6e4
JosephTLockwood <joeojazz@gmail.com>
2023-03-06 20:41:27 -0500

    Add cube options

---
+++ src/main/java/frc/robot/util/gamePiecePicker.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+/** Add your docs here. */
+public class gamePiecePicker {
+  static boolean gamePiecePicked = true;
+
+  public static void toggle(boolean i){
+    if(i){
+      gamePiecePicked = true;
+    }
+    else{
+      gamePiecePicked = false;
+    }
+  }
+
+  public static boolean getPiecePicker(){
+    return gamePiecePicked;
+  }
+
+
+}
